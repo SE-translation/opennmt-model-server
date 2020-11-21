cfb17657d7c45b818dc76f7346f7b06a27c17e41
Quinten <tintin10q@hotmail.com>
2020-11-21 01:29:21 +0100

    Loaded app and application from load instead of doing main() 🦅

---
+++ application.py
@@ -0,0 +1,10 @@
+from load import app, application, translation_server, parsed_args
+application
+app
+translation_server.start(parsed_args.config)
+print("started model server")
+
+if __name__ == '__main__':
+    print("started model server")
+    app.run(port=1600)
+
